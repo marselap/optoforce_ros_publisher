85fb2fed243b57d69202e3428933175a3b178851
Jelena <jelena.tabak@gmail.com>
2020-11-11 17:46:52 +0100

    add gains

--- src/optoforce_sensor_node.cpp
+++ src/optoforce_sensor_node.cpp
@@ -22,6 +22,9 @@ int main(int argc, char **argv)
 
     double fx_gain, fy_gain, fz_gain;  // sensitivity gain in counts/N
     double tx_gain, ty_gain, tz_gain;  // sensitivity gain in count/Nm
+		double fx_gain_3D[4], fy_gain_3D[4], fz_gain_3D[4];  // sensitivity gains for 3D sensors
+
+		u_int8_t sens_params_set[4];
 
     int speed, filter;
 		int n_sensors;
@@ -37,7 +40,6 @@ int main(int argc, char **argv)
         ros::NodeHandle n("~");
 
 				n.param("n_sensors", n_sensors, 1);
-				u_int8_t sens_params_set = 1;
 
         //Creating wrench publisher
         ros::Publisher wrench_pub_raw[n_sensors];
@@ -53,27 +55,47 @@ int main(int argc, char **argv)
 
 			 	}
 
-        // get sensitivy gains from rosparam server
-        n.param("fx_gain", fx_gain, 0.0);
-        n.param("fy_gain", fy_gain, 0.0);
-        n.param("fz_gain", fz_gain, 0.0);
+				// get sensitivy gains from rosparam server
+				// check the order in which the sensors are connected!
+				n.param("ISE_23_fx_gain", fx_gain_3D[0], 0.0);
+				n.param("ISE_23_fy_gain", fy_gain_3D[0], 0.0);
+				n.param("ISE_23_fz_gain", fz_gain_3D[0], 0.0);
+				n.param("ISE_30_fx_gain", fx_gain_3D[1], 0.0);
+				n.param("ISE_30_fy_gain", fy_gain_3D[1], 0.0);
+				n.param("ISE_30_fz_gain", fz_gain_3D[1], 0.0);
+				n.param("ISE_31_fx_gain", fx_gain_3D[2], 0.0);
+				n.param("ISE_31_fy_gain", fy_gain_3D[2], 0.0);
+				n.param("ISE_31_fz_gain", fz_gain_3D[2], 0.0);
+				n.param("ISE_29_fx_gain", fx_gain_3D[3], 0.0);
+				n.param("ISE_29_fy_gain", fy_gain_3D[3], 0.0);
+				n.param("ISE_29_fz_gain", fz_gain_3D[3], 0.0);
 
-        n.param("tx_gain", tx_gain, 0.0);
-        n.param("ty_gain", ty_gain, 0.0);
-        n.param("tz_gain", tz_gain, 0.0);
+				n.param("fx_gain", fx_gain, 0.0);
+				n.param("fy_gain", fy_gain, 0.0);
+				n.param("fz_gain", fz_gain, 0.0);
 
-        n.param("speed", speed, 1000);
-        n.param("filter", filter, 150);
+				n.param("tx_gain", tx_gain, 0.0);
+				n.param("ty_gain", ty_gain, 0.0);
+				n.param("tz_gain", tz_gain, 0.0);
+
+				n.param("speed", speed, 1000);
+				n.param("filter", filter, 150);
 
         double double_thresh = 0.0001;
         if (daq.getVersion()!=_95 && daq.getVersion() != _64) // It is a 3D sensor
         {
-    				std::cout<<"Setting up the sensors"<<std::endl;
+						std::cout<<"Found 3DOF sensor -> Starting ROS node"<<std::endl;
 
-            if (fx_gain < 0.0001 || fy_gain < 0.0001 || fz_gain < 0.001) {
-                std::cout<<"Sensitivity force params not set properly. Publishing only raw values."<<std::endl;
-                sens_params_set = 0;
-            }
+						for (int i = 0; i < n_sensors; i++)
+						{
+	            if (fx_gain_3D[i] < 0.0001 || fy_gain_3D[i] < 0.0001 || fz_gain_3D[i] < 0.001) {
+	                std::cout<<"Sensitivity force params for sensor on port " << i << " not set properly. Publishing only raw values."<<std::endl;
+	                sens_params_set[i] = 0;
+	            }
+							else {
+								sens_params_set[i] = 1;
+							}
+						}
 
             config = daq.getConfig();
 
@@ -120,11 +142,11 @@ int main(int argc, char **argv)
 	                wrench_msg.wrench.torque.z = 0.0;
 	                wrench_pub_raw[i].publish(wrench_msg);
 
-	                if (sens_params_set) {
+	                if (sens_params_set[i]) {
 	                    wrench_msg.header.stamp = ros::Time::now();
-	                    wrench_msg.wrench.force.x = pack3D.x / fx_gain;
-	                    wrench_msg.wrench.force.y = pack3D.y / fy_gain;
-	                    wrench_msg.wrench.force.z = pack3D.z / fz_gain;
+	                    wrench_msg.wrench.force.x = pack3D.x / fx_gain_3D[i];
+	                    wrench_msg.wrench.force.y = pack3D.y / fy_gain_3D[i];
+	                    wrench_msg.wrench.force.z = pack3D.z / fz_gain_3D[i];
 	                    wrench_msg.wrench.torque.x = 0.0;
 	                    wrench_msg.wrench.torque.y = 0.0;
 	                    wrench_msg.wrench.torque.z = 0.0;
@@ -138,19 +160,20 @@ int main(int argc, char **argv)
         }
         else // It is a 6D sensor = the only one supported for now
         {
+						sens_params_set[0] = 1;
             OptoPackage6D pack6D;
-            int size=daq.read6D(pack6D,false);
+
             //init ROS node
             std::cout<<"Found 6DOF sensor -> Starting ROS node"<<std::endl;
 
             if (fx_gain < 0.0001 || fy_gain < 0.0001 || fz_gain < 0.001) {
                 std::cout<<"Sensitivity force params not set properly. Publishing only raw values."<<std::endl;
-                sens_params_set = 0;
+                sens_params_set[0] = 0;
             }
 
             if (tx_gain < 0.0001 || ty_gain < 0.0001 || tz_gain < 0.001) {
                 std::cout<<"Sensitivity torque params not set properly.Publishing only raw values."<<std::endl;
-                sens_params_set = 0;
+                sens_params_set[0] = 0;
             }
 
             config = daq.getConfig();
@@ -204,7 +227,7 @@ int main(int argc, char **argv)
 
                 ros::spinOnce();
                 loop_rate.sleep();
-                size = daq.read6D(pack6D,false);
+                daq.read6D(pack6D,false);
                 //std::cout<<size<<std::endl;
             }
             //std::cout<<"Fx: "<<pack6D.Fx<<" Fy: "<<pack6D.Fy<<" Fz: "<<pack6D.Fz<<" ";
