718df203a486105c2d56c50318ab8a0276600d2a
Jelena <jelena.tabak@gmail.com>
2020-11-11 15:10:56 +0100

    enable multiple 3D sensors

--- src/optoforce_sensor_node.cpp
+++ src/optoforce_sensor_node.cpp
@@ -13,7 +13,7 @@ void msSleep(unsigned long p_uMillisecs)
 
 int main(int argc, char **argv)
 {
-        
+
     OptoDAQ daq;
     OptoPorts ports;
     msSleep(2500); // We wait some ms to be sure about OptoPorts enumerated PortList
@@ -24,23 +24,34 @@ int main(int argc, char **argv)
     double tx_gain, ty_gain, tz_gain;  // sensitivity gain in count/Nm
 
     int speed, filter;
-
-    u_int8_t sens_params_set = 1;
+		int n_sensors;
 
     OPort* portlist=ports.listPorts(true);
 
-
     if (ports.getLastSize()>0)
     {
-    	daq.open(portlist[0]);
+    		daq.open(portlist[0]);
         daq.zeroAll();
 
         ros::init(argc, argv, "OForceSensorPublisher");
         ros::NodeHandle n("~");
-            
+
+				n.param("n_sensors", n_sensors, 1);
+				u_int8_t sens_params_set = 1;
+
         //Creating wrench publisher
-        ros::Publisher wrench_pub_raw = n.advertise<geometry_msgs::WrenchStamped>("OptoForceWrench_raw", 1000);
-        ros::Publisher wrench_pub = n.advertise<geometry_msgs::WrenchStamped>("OptoForceWrench", 1000);
+        ros::Publisher wrench_pub_raw[n_sensors];
+        ros::Publisher wrench_pub[n_sensors];
+
+				for(int i = 0; i < n_sensors; i++)
+				{
+					std::string topic_raw = "OptoForceWrench_raw_" + std::to_string(i);
+					std::string topic = "OptoForceWrench_" + std::to_string(i);
+
+					wrench_pub_raw[i] = n.advertise<geometry_msgs::WrenchStamped>(topic_raw, 1000);
+					wrench_pub[i] = n.advertise<geometry_msgs::WrenchStamped>(topic, 1000);
+
+			 	}
 
         // get sensitivy gains from rosparam server
         n.param("fx_gain", fx_gain, 0.0);
@@ -55,12 +66,9 @@ int main(int argc, char **argv)
         n.param("filter", filter, 150);
 
         double double_thresh = 0.0001;
-
         if (daq.getVersion()!=_95 && daq.getVersion() != _64) // It is a 3D sensor
         {
-            OptoPackage pack3D;
-            int size=daq.read(pack3D,false);	// Reading Sensor #0 (up to 16 Sensors)
-            std::cout<<"Found 3DOF sensor -> Starting ROS node"<<std::endl;
+    				std::cout<<"Setting up the sensors"<<std::endl;
 
             if (fx_gain < 0.0001 || fy_gain < 0.0001 || fz_gain < 0.001) {
                 std::cout<<"Sensitivity force params not set properly. Publishing only raw values."<<std::endl;
@@ -92,34 +100,39 @@ int main(int argc, char **argv)
             //Set ROS rate to speed Hz
             ros::Rate loop_rate(speed);
             //Main ROS loop
+
+						OptoPackage pack3D;
             while(ros::ok())
             {
                 ros::spinOnce();
-                int size=daq.read(pack3D,false);
+								for (int i = 0; i < n_sensors; i++)
+								{
+	                daq.read(pack3D,i,false);
 
-                geometry_msgs::WrenchStamped wrench_msg; // Create msg
-                //Fill msg
-                wrench_msg.header.stamp = ros::Time::now();
-                wrench_msg.wrench.force.x = pack3D.x;
-                wrench_msg.wrench.force.y = pack3D.y;
-                wrench_msg.wrench.force.z = pack3D.z;
-                wrench_msg.wrench.torque.x = 0.0;
-                wrench_msg.wrench.torque.y = 0.0;
-                wrench_msg.wrench.torque.z = 0.0;
-                wrench_pub_raw.publish(wrench_msg);
+	                geometry_msgs::WrenchStamped wrench_msg; // Create msg
+	                //Fill msg
+	                wrench_msg.header.stamp = ros::Time::now();
+	                wrench_msg.wrench.force.x = pack3D.x;
+	                wrench_msg.wrench.force.y = pack3D.y;
+	                wrench_msg.wrench.force.z = pack3D.z;
+	                wrench_msg.wrench.torque.x = 0.0;
+	                wrench_msg.wrench.torque.y = 0.0;
+	                wrench_msg.wrench.torque.z = 0.0;
+	                wrench_pub_raw[i].publish(wrench_msg);
 
-                if (sens_params_set) {
-                    wrench_msg.header.stamp = ros::Time::now();
-                    wrench_msg.wrench.force.x = pack3D.x / fx_gain;
-                    wrench_msg.wrench.force.y = pack3D.y / fy_gain;
-                    wrench_msg.wrench.force.z = pack3D.z / fz_gain;
-                    wrench_msg.wrench.torque.x = 0.0;
-                    wrench_msg.wrench.torque.y = 0.0;
-                    wrench_msg.wrench.torque.z = 0.0;
-                    wrench_pub.publish(wrench_msg);
-                }
+	                if (sens_params_set) {
+	                    wrench_msg.header.stamp = ros::Time::now();
+	                    wrench_msg.wrench.force.x = pack3D.x / fx_gain;
+	                    wrench_msg.wrench.force.y = pack3D.y / fy_gain;
+	                    wrench_msg.wrench.force.z = pack3D.z / fz_gain;
+	                    wrench_msg.wrench.torque.x = 0.0;
+	                    wrench_msg.wrench.torque.y = 0.0;
+	                    wrench_msg.wrench.torque.z = 0.0;
+	                    wrench_pub[i].publish(wrench_msg);
+	                }
 
-                loop_rate.sleep();
+	                loop_rate.sleep();
+								}
             }
 
         }
@@ -176,7 +189,7 @@ int main(int argc, char **argv)
                 wrench_msg.wrench.torque.x = pack6D.Tx;
                 wrench_msg.wrench.torque.y = pack6D.Ty;
                 wrench_msg.wrench.torque.z = pack6D.Tz;
-                wrench_pub_raw.publish(wrench_msg);
+                wrench_pub_raw[0].publish(wrench_msg);
 
                 if (sens_params_set) {
                     wrench_msg.header.stamp = ros::Time::now();
@@ -186,7 +199,7 @@ int main(int argc, char **argv)
                     wrench_msg.wrench.torque.x = pack6D.Tx / tx_gain;
                     wrench_msg.wrench.torque.y = pack6D.Ty / ty_gain;
                     wrench_msg.wrench.torque.z = pack6D.Tz / tz_gain;
-                    wrench_pub.publish(wrench_msg);
+                    wrench_pub[0].publish(wrench_msg);
                 }
 
                 ros::spinOnce();
